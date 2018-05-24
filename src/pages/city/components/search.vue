d0daa090025d2eb14cd825fa617d9916b79182a3
LucyGirl999 <LucyGirl999@163.com>
2018-05-24 22:16:45 +0800

    city seach

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+</template>
+
+<script>
+export default{
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .search
+      background: $bgColor
+      padding: 0 .1rem
+      height: .72rem
+      .search-input
+        box-sizing: border-box
+        width: 100%
+        height: .62rem
+        text-align: center
+        line-height: .62rem
+        color: #666
+        border-radius: .06rem
+        padding: 0 .1rem
+</style>
