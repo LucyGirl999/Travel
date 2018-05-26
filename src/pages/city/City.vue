583098c1c19db56e88e8f54a806fbc8201a2c686
LucyGirl999 <LucyGirl999@163.com>
2018-05-26 15:22:20 +0800

    commit city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-letter></city-letter>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/header'
 import CitySearch from './components/search'
+import CityList from './components/list'
+import CityLetter from './components/Letter'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityLetter
   }
 }
 </script>
---
+++ src/pages/city/components/Letter.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul class="letter">
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">C</li>
+        <li class="item">D</li>
+        <li class="item">E</li>
+        <li class="item">F</li>
+        <li class="item">G</li>
+        <li class="item">H</li>
+        <li class="item">I</li>
+        <li class="item">J</li>
+        <li class="item">K</li>
+        <li class="item">M</li>
+        <li class="item">N</li>
+        <li class="item">O</li>
+        <li class="item">P</li>
+        <li class="item">Q</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityLetter'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .letter
+        display: flex
+        flex-direction: column
+        justify-content: center
+        position: absolute
+        top: 1.58rem
+        right: 0
+        bottom: 0
+        width: .4rem
+        .item
+            text-align: center
+            line-height: .4rem
+            color: $bgColor
+</style>
