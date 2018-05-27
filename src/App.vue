b90d508652621ec18656c7fe61505d2275f59275
LucyGirl999 <LucyGirl999@163.com>
2018-05-27 13:34:23 +0800

    keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <!-- <router-view>显示当前路由地址下的内容 -->
-    <router-view/>
+    <!-- 使用keep-alive标签包含起来是为了在ajax请求是，将返回内容保存起来，在内容没有发生改变时，不在请求数据 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
